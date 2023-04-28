72877b8738c282b7b051911111bc4417016c8955
myfootsasleep <richardoda@gmail.com>
2023-04-27 22:56:07 -0700

    added a new file called index.js where we run all the input from there. Main file only runs the request. Added mocha chai to test as well

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,16 @@
 const request = require("request");
-const breed = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error,null);
+    }
+    const data = JSON.parse(body);
+    if (data[0]) {
+      callback(null,data[0]["description"]);
+    } else {
+      callback(null, "");
+    }
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
-  // console.log('error:', error);
-  // console.log('statusCode:', response && response.statusCode);
-  //console.log('body: ', body);
-  const data = JSON.parse(body);
-  //console.log(data);
-  console.log(data['0']['description']);
-});
+module.exports = { fetchBreedDescription};
